b7cfe30bb5b66b0c67152f58cbecdd81dd646f5b
OdCheban <odcheban@gmail.com>
2019-03-28 16:54:47 +0300

    try catch

--- Assets/Scripts/Game/Online/PlayerOnline.cs
+++ Assets/Scripts/Game/Online/PlayerOnline.cs
@@ -66,23 +66,44 @@ public class PlayerOnline : CellUp
     [Command]
     public void CmdNext(GameObject target, int i, int j)
     {
-        CellUp cellUp = target.GetComponent<CellUp>();
-        cellUp.nextI = i;
-        cellUp.nextJ = j;
+        try
+        {
+            CellUp cellUp = target.GetComponent<CellUp>();
+            cellUp.nextI = i;
+            cellUp.nextJ = j;
+        }
+        catch
+        {
+
+        }
     }
     [Command]
     public void CmdPos(GameObject target, int ii, int jj)
     {
+        try
+        { 
         CellUp cellTarget = target.GetComponent<CellUp>();
         cellTarget.iPos = ii;
         cellTarget.jPos = jj;
+        }
+        catch
+        {
+
+        }
     }
     [Command]
     public void CmdTarget(GameObject target, int i, int j)
     {
+        try
+        {
         CellUp cellUp = target.GetComponent<CellUp>();
         cellUp.iTarget = i;
         cellUp.jTarget = j;
+        }
+        catch
+        {
+
+        }
     }
 
     [Command]
